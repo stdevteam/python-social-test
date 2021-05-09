f31d29a13a6d5028d0c7375085ec07f39ffb8164
Pargev <pargev@stdevmail.com>
2021-05-09 21:34:11 +0400

    added cors handler

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from settings import settings
 from apps.post.app import post_app
 
-
 app = Flask(__name__)
 app.register_blueprint(post_app)
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=settings.DEBUG)
