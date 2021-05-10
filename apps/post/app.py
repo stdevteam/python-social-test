c0b40599f26fca980540a239dc4a45f6d87e2750
Pargev Aghabekyan <pargev@stdevmail.com>
2021-05-10 13:20:06 +0400

    added comments get

--- apps/post/app.py
+++ apps/post/app.py
@@ -6,11 +6,14 @@ post_app = Blueprint('post_app', __name__)
 
 
 @post_app.route('/posts/', methods=["GET", "POST"], defaults={'id': None})
-@post_app.route('/posts/<int:id>/', methods=["DELETE", "PUT"])
+@post_app.route('/posts/<int:id>/', methods=["DELETE", "PUT", "GET"])
 def post(id):
     if request.method == "GET":
         # TODO Add pagination
-        return jsonify(Post().get_posts_with_reactions_count())
+        if id:
+            return jsonify(Post().get(id=id))
+        else:
+            return jsonify(Post().get_posts_with_reactions_count())
     elif request.method == "POST":
         return jsonify(Post().create(**request.form))
     elif request.method == "DELETE":
@@ -18,12 +21,12 @@ def post(id):
             Post().delete(id=id)
             return jsonify({})
         except Exception as e:
-            return jsonify({'error': str(e)})
+            return jsonify({'error': str(e)}), 422
     elif request.method == "PUT":
         try:
             return jsonify(Post().update({'id': id}, **request.form))
         except Exception as e:
-            return jsonify({'error': str(e)})
+            return jsonify({'error': str(e)}), 422
 
 
 @post_app.route('/react/', methods=["POST"])
@@ -31,12 +34,16 @@ def react():
     try:
         return jsonify(Reaction().create(**request.form))
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 422
 
 
-@post_app.route('/comment/', methods=["POST"])
-def comment():
-    try:
-        return jsonify(Comment().create(**request.form))
-    except Exception as e:
-        return jsonify({'error': str(e)})
+@post_app.route('/comment/', methods=["POST"], defaults={'post_id': None})
+@post_app.route('/comment/<int:post_id>/', methods=["GET"])
+def comment(post_id):
+    if request.method == "POST":
+        try:
+            return jsonify(Comment().create(**request.form))
+        except Exception as e:
+            return jsonify({'error': str(e)}), 422
+    elif request.method == "GET":
+        return jsonify(Comment().filter(post_id=post_id))
